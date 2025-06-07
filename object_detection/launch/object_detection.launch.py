d861cf80833b9a6918d10923c67c135af7ff47ae
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-06-07 10:44:27 +0000

    Added script to detect the object position

--- object_detection/launch/object_detection.launch.py
+++ object_detection/launch/object_detection.launch.py
@@ -6,6 +6,11 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
+    package_description = "object_detection"
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'object_detection_config.rviz')
+
+    
     static_tf_pub_node = Node(
         name="static_tf_publisher",
         package="object_detection",
@@ -13,8 +18,26 @@ def generate_launch_description():
         output="screen",    
     )
 
+    object_detection_node = Node(
+        name="objects_detection",
+        package="object_detection",
+        executable="objects_detection.py",
+        output="screen",    
+    )
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
     return LaunchDescription(
         [
+            rviz_node,
             static_tf_pub_node,
+            object_detection_node,
+            
         ]
     )
