7f921e30924709ca122b7074c5ee2bc879dc6a10
Hajar Bartakh <bartakh.hajar@gmail.com>
2025-06-11 10:59:22 +0000

    Replaced launching individual nodes by a nested launch file

--- moveit2_scripts/launch/pick_and_place_perception_real.launch.py
+++ moveit2_scripts/launch/pick_and_place_perception_real.launch.py
@@ -3,25 +3,22 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from moveit_configs_utils import MoveItConfigsBuilder
-from launch.actions import TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
 
-    static_tf_pub_node = Node(
-        name="static_tf_publisher_real",
-        package="object_detection",
-        executable="static_tf_publisher_real.py",
-        output="screen",    
-    )
-
-    # This node will publish the cube's coordinates 
-    # so the robot arm can pick and place it
-    object_detection_node = Node(
-        name="objects_detection_real",
-        package="object_detection",
-        executable="objects_detection_real.py",
-        output="screen", 
+    
+    # Launch the object detection nodes first to start publishing the object position
+    object_detection_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('object_detection'),
+                'launch',
+                'object_detection_real.launch.py'
+            )
+        )
     )
 
     moveit_config = MoveItConfigsBuilder("name", package_name="real_moveit_config").to_moveit_configs()
@@ -41,19 +38,17 @@ def generate_launch_description():
         emulate_tty = True,
     )
 
-    # wait for 5 seconds to make sure the detected objects position
-    # is being published then launch the subscriber node
+    # wait for 10 seconds to make sure the detected objects position
+    # is being published then launch the subscriber node and the moveit node
     delayed_moveit_node = TimerAction(
-        period=5.0,  
+        period=10.0,  
         actions=[
         moveit_cpp_node,
         ])
 
     return LaunchDescription(
         [
-            # static_tf_pub_node,
-            # object_detection_node,
-            # moveit_cpp_node,
+            object_detection_launch,
             delayed_moveit_node,
         ]
     )
